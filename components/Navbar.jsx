1be5d7a677dba29dc5ad9806d1ea9d9281ae3e41
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2023-04-11 22:09:22 +0200

    links added to navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Image from "next/image"
-
+import Link from "next/link"
     const Navbar = () => {
-        return (<div className="fixed w-full h-20 shadow-xl z-[100]">
-            <div className="flex justify-between items-center w-full px-2 2xl:px-16">
-            <Image src="/../public/assets/logo.png" alt="Frédéric Gaufichon" width="135" height="60" />
+        return (<div className="fixed w-full h-20 shadow-xl z-[100] border border-green-800">
+            <div className="flex justify-between items-center w-full px-2 2xl:px-16 pt-1 border border-blue-800">
+            <Image src="/../public/assets/logo.png" alt="Frédéric Gaufichon" width="180" height="90" className="border border-red-800"/>
+            <div>
+                <ul className="hidden md:flex">
+                    <Link href='/'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+                    </Link><Link href='/'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">Skils</li>
+                    </Link><Link href='/'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
+                    </Link>
+                </ul>
+            </div>
             </div>
         </div>)
 }
