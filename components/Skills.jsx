84c94088a993607fd43cc55273176c990832197e
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2023-04-18 14:03:10 +0200

    try component Skill

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-
+import Image from 'next/image';
+import Skill from './Skill'
     const Skills = () => {
         return(
-            <div>
-                skills
+            <div className="w-full lg:h-screen p-2">
+                <div className="max-w-[1240px] mx-auto flex flex-col justify-self-center h-full">
+                    <p className='uppercase text-xl tracking-widest text-[#5651e5]'>Skills</p>
+                    <h2 className='py-4'>What I Can Do</h2>
+                    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+
+                        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                                <div className="m-auto">
+                                    <Image src="/skills/React-icon.svg" alt="react"layout="responsive" width='64' height='64'/>
+                                </div>
+                                <div className="flex flex-col items-center justify-center">
+                                    <h3>REACT</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <Skill props={'React-icon.svg'} />
+                    </div>                  
+                </div>
             </div>
         )
     }
