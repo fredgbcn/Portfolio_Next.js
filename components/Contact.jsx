471aeb5aea5a6422e268bf00e8632a08c39c6e24
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2023-04-19 17:27:14 +0200

    bases contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,71 +3,91 @@ import Image from "next/image";
 import {FaLinkedinIn, FaGithub} from 'react-icons/fa'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import {AiOutlineMail} from 'react-icons/ai'
-
+import Link from "next/link";
+import ContactImg from '/public/assets/contact.png';
     const Contact = (props) => {
         return(
-            <div className="w-full lg:h-screen">
-                <div className="max-x-[1240px] m-auto px-2 py-16 w-full">
-                    <p className="text-xl tracking-widest uppercase text-[#5651e5]">
-                        Contact
-                    </p>
-                    <h2 className="py-4">Get In Touch</h2>
-                    <div className="grid lg:grid-cols-5 gap-8">
-
-                        <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
-                            <div className="lg:p-4 h-full">
-                                <div>
-                                    <Image className="rounded-xl hover:scale-105 ease-in duration-300" src="/../public/assets/contact.png" width={200} height={200} alt="contact"/>
-                                </div>
-                                <div>
-                                    <h2 className="py-2">Frédéric Gaufichon</h2>
-                                    <p>Front End Developer</p>
-                                    <p className="py-4">I am available for freelance or full-time positions. Do not hesitate to contact me to talk about it</p>
-                                </div>
-                            </div>
-                            <div className="flex items-center justify-between py-4">
-                                <p className="uppercase pt-8">Connect With Me</p>
-                                <div>
-                                 <a href='https://www.linkedin.com/'
-                                    target='_blank'
-                                    rel='noreferrer'
-                                    >
-                                     <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                                        <FaLinkedinIn fill="#6C47B2"/>
-                                    </div>
-                                 </a>
-                                 <a
-                                    href='https://github.com/fredgbcn'
-                                    target='_blank'
-                                    rel='noreferrer'
-                                    >
-                                     <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                                        <FaGithub fill="#6C47B2" />
-                                     </div>
-                                  </a>
-                                  <a href='https://www.linkedin.com/'
-                                    target='_blank'
-                                    rel='noreferrer'
-                                    >
-                                     <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                                        <AiOutlineMail fill="#6C47B2"/>
-                                     </div>
-                                   </a>
-                                   <a href='https://www.linkedin.com/'
-                                    target='_blank'
-                                    rel='noreferrer'
-                                    >
-                                     <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                                        <BsFillPersonLinesFill fill="#6C47B2"/>
-                                     </div>
-                                  </a>
-                                </div>
-                            </div>
-                        </div>
-
+            <div id='contact' className='w-full lg:h-screen'>
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Contact
+        </p>
+        <h2 className='py-4'>Get In Touch</h2>
+        <div className='grid lg:grid-cols-5 gap-8'>
+          {/* left */}
+          <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+            <div className='lg:p-4 h-full '>
+              <div>
+                <Image
+                  className='rounded-xl hover:scale-105 ease-in duration-300'
+                  src={ContactImg}
+                  alt='/'
+                />
+              </div>
+              <div>
+                <h2 className='py-2'>Frédéric Gaufichon</h2>
+                <p>Front-End Developer</p>
+                <p className='py-4'>
+                  I am available for freelance or full-time positions. Do not hesitate to contact me to talk about it
+                </p>
+              </div>
+              <div>
+                <p className='uppercase pt-8'>Connect With Me</p>
+                <div className='flex items-center justify-between py-4'>
+                  <a
+                    href='https://www.linkedin.com/in/clint-briley-50056920a/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaLinkedinIn />
                     </div>
+                  </a>
+                  <a
+                    href='https://github.com/fireclint'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaGithub />
+                    </div>
+                  </a>
+
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <AiOutlineMail />
+                  </div>
+                  <Link href='/resume'>
+                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <BsFillPersonLinesFill />
+                      </div>
+                  </Link>
                 </div>
+              </div>
             </div>
+          </div>
+             {/* right */}
+        <div>
+            <form
+                action="https://getform.io/f/79ecc087-6b08-4aed-9c9f-8228f79d9ced"
+                method="POST"
+                encType='multipart/form-data'>
+                <div>
+                    <label>NAME</label>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                    <label>PHONE NUMBER</label>
+                    <input type="text" name="phone" />
+                </div>
+                <button type="submit">
+                  Send Message
+                </button>
+            </form>
+        </div>
+
+          </div>
+          </div>
+          </div>
         )
     }
 
