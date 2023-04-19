483163dd40ca94c2b966cb3d913f973ac155a917
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2023-04-19 14:07:01 +0200

    finish projects part

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-import Image from 'next/image'
-import Link from "next/link";
+import ProjectItem from "./ProjectItem"
+import AfricaImg from "../public/assets/africa1.png"
+import CSS from "../public/assets/m-i-m1.png"
+import FlagQuiz from "../public/assets/flags.png"
+import DevisPlus from "../public/assets/Devis-Plus.png"
+import EAH from "../public/assets/E-a-h.png"
+import Tcloc from "../public/assets/tcloc1.png"
+import FlagDB from "../public/assets/Flags1.png"
+
+
     const Projects = () => {
         return(
             <div className="w-full">
@@ -8,17 +16,44 @@ import Link from "next/link";
                     <p className='uppercase text-xl tracking-widest text-[#5651e5]'>Projects</p>
                     <h2 className='py-4'>What I Can Do</h2>
                     <div className="grid md:grid-cols-2 gap-8">
-
-                    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-                            <Image src="/../public/assets/africa1.png" className="rounded-xl group-hover:opacity-10" layout="responsive" width={200} height={200} alt="Frédéric Gaufichon" />
-                            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                                <h3 className='text-2xl text-white tracking-wider text-center'>Website from Scratch</h3>
-                                <p className='pb-4 pt-2 text-white text-center'>React/Node and Mongodb</p>
-                                <Link href='/'>
-                                    <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
-                                </Link>
-                            </div>
-                        </div>
+                        <ProjectItem
+                            title='Website From Scratch'
+                            backgroundImg={AfricaImg}
+                            projectUrl='/africa'
+                            tech='React/Node JS and MongoDB'
+                        />
+                        <ProjectItem
+                            title='CSS Animations Tutorial'
+                            backgroundImg={CSS}
+                            projectUrl='/MIM'
+                            tech='VUE.JS and CSS Animations'
+                        /><ProjectItem
+                            title='Flag Quiz'
+                            backgroundImg={FlagQuiz}
+                            projectUrl='Flag-quiz'
+                            tech='Typescript'
+                        /><ProjectItem
+                            title='SEO Audit'
+                            backgroundImg={DevisPlus}
+                            projectUrl='Devis-Plus'
+                            tech='SemRush, Spyfu, Analytics...'
+                        /><ProjectItem
+                            title='Rock Band Portfolio'
+                            backgroundImg={EAH}
+                            projectUrl='EAH'
+                            tech='Wordpress'
+                        /><ProjectItem
+                            title='Manager, Writer and SEO'
+                            backgroundImg={Tcloc}
+                            projectUrl='Tcloc'
+                            tech='Semrush, Spyfu, YOAST'
+                        /><ProjectItem
+                            title='Flag Database'
+                            backgroundImg={FlagDB}
+                            projectUrl='Flag-Database'
+                            tech='React.JS'
+                        />
+                    
                     </div>
                 </div>
             </div>
