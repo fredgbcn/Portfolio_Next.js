ea37dac3ec05745ff146a56c6cdfe198232b6431
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2023-04-20 13:38:49 +0200

    description page for africa begue

---
+++ pages/africa.jsx
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import React from 'react';
+import africaImg from "../public/assets/africa1.png"
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
+
+const property = () => {
+  return (
+    <div className='w-full'>
+      <div className='w-screen h-[50vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+        <Image
+          className='absolute z-1'
+          layout='fill'
+          objectFit='cover'
+          src={africaImg}
+          alt='/'
+        />
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+          <h2 className='py-2'>Website of an association From Scratch</h2>
+          <h3>React JS / Node JS / MongoDB</h3>
+        </div>
+      </div>
+
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+        <div className='col-span-4'>
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+          This website should show the dance and humanitarian activities of the association "Africa Begue" and allow the persons in charge to modify the events of the "Stage" part. I created the design with Figma inspired by Senegalese fabrics. For the administrator part, I created a form linked to a Node.js backend that allows the association to manage the events themselves and to store them on MongoDB
+          </p>
+          <a
+            href='https://github.com/fredgbcn/africa_begue'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+          </a>
+          <a
+            href='http://africabegue.com/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4'>Demo</button>
+          </a>
+        </div>
+        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+          <div className='p-2'>
+            <p className='text-center font-bold pb-2'>Technologies</p>
+            <div className='grid grid-cols-3 md:grid-cols-1'>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> React
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Node JS
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Javascript
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> MongoDB
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Figma
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href='/#projects'>
+          <p className='underline cursor-pointer'>Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default property;
