51e901dddf322a22e156aa82507a97e3a1aa1a0e
Frédéric Gaufichon <64479936+fredgbcn@users.noreply.github.com>
2025-01-03 00:31:15 +0100

    Update mim.jsx

--- pages/mim.jsx
+++ pages/mim.jsx
@@ -36,7 +36,7 @@ const property = () => {
             <button className='px-8 py-2 mt-4 mr-8'>Code</button>
           </a>
           <a
-            href='https://fredgbcn.github.io/mim/index.html'
+            href='https://www.fredgbcn.github.io/mim/index.html'
             target='_blank'
             rel='noreferrer'
           >
